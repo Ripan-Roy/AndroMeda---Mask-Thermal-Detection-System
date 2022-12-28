c838e1c49d51038238536918e1961b2fe0909a7b
RownakM <rmazumder1033@gmail.com>
2022-12-28 11:34:45 +0530

    Frontend

--- video_stream/settings.py
+++ video_stream/settings.py
@@ -117,5 +117,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
